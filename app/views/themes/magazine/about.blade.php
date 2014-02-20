099b9b9797a7e481cc9d86aee9e5b7d9bc499730
Reinier Ladan <reinierl@gmail.com>
2014-02-20 17:05:24 +0100

    About The Panel

--- app/views/themes/magazine/about.blade.php
+++ app/views/themes/magazine/about.blade.php
@@ -10,6 +10,7 @@
 
 @section('content')
 	<!-- insert your about text here -->
+	<p>This site runs on <a href="https://github.com/reinier/ThePanel">The Panel</a>, a web service to curate links with a small crowd.</p>
 	<h3>@lang('magazine.contributors', array('sitetitle' => Config::get('site.title')))</h3>
 	<ul class="bios">
 			@foreach ($users as $user_id => $user)

f0612f81d0eee2683b4f172d59281fd6db91148b
Reinier Ladan <reinierl@gmail.com>
2014-02-20 19:24:09 +0100

    A small start Dutch language support

---
+++ app/lang/nl/magazine.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+	"about" => "Over :sitetitle",
+	"contributors" => "Deelnemers aan :sitetitle",
+	"aboutcontent" => '<p>Deze site maakt gebruik van <a href="https://github.com/reinier/ThePanel">The Panel</a>, een webdienst om met een kleine groep mensen links te verzamelen.</p>',
+);
